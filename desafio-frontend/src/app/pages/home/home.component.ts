3d79508a2e313dd883511d86dce00e7f48d99bf1
raunerlucas <raunerlucas@gmail.com>
2024-09-11 16:56:14 -0300

    feat: footer

--- desafio-frontend/src/app/pages/home/home.component.ts
+++ desafio-frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MainComponent } from "../../components/templates/main/main.component";
+import { FooterComponent } from "../../components/templates/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MainComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
